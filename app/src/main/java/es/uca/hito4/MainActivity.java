2706b277651de8fd0cd10c96a0261cc7b3886457
Antonio José  Sánchez <ajsamu@hotmail.com>
2020-05-10 18:26:49 +0200

    añadida paleta de colores y menu

--- app/src/main/java/es/uca/hito4/MainActivity.java
+++ app/src/main/java/es/uca/hito4/MainActivity.java
@@ -44,8 +44,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_asistentes, R.id.nav_programa, R.id.nav_fechas, R.id.nav_localizacion)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
