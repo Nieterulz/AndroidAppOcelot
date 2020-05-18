9424a29dff5207c4a35612a9ab322002db7d236a
lui <luisdecelis3@gmail.com>
2020-05-18 19:54:52 +0200

    cositas

---
+++ app/src/main/java/es/uca/hito4/Constants.java
@@ -0,0 +1,5 @@
+package es.uca.hito4;
+
+public class Constants {
+    public static final String MAPVIEW_BUNDLE_KEY = "AIzaSyDh-N_TpIqE67TaPrgkxKNcP3GiIvGDsfA";
+}
--- app/src/main/java/es/uca/hito4/ui/localizacion/LocalizacionFragment.java
+++ app/src/main/java/es/uca/hito4/ui/localizacion/LocalizacionFragment.java
@@ -1,5 +1,7 @@
 package es.uca.hito4.ui.localizacion;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,27 +10,101 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import es.uca.hito4.Constants;
 import es.uca.hito4.R;
 
-public class LocalizacionFragment extends Fragment {
+public class LocalizacionFragment extends Fragment implements OnMapReadyCallback{
     private LocalizacionViewModel localizacionViewModel;
+    private MapView mMapView;
+    private Constants constantes;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        localizacionViewModel =
-                ViewModelProviders.of(this).get(LocalizacionViewModel.class);
+
+
         View root = inflater.inflate(R.layout.fragment_localizacion, container, false);
-        final TextView textView = root.findViewById(R.id.text_localizacion);
-        localizacionViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        mMapView = root.findViewById(R.id.map);
+        initGoogleMap(savedInstanceState);
+
         return root;
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState){
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(constantes.MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(constantes.MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(constantes.MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("hola"));
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
